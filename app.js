c12609465e1e13a5177c6e56669b0ba51c9c71ad
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-04 20:38:21 +0100

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-// Keep Alive server for Render
+const { Bot } = require("grammy");
+const bot = new Bot(process.env.BOT_TOKEN);
+
+// Commands
+bot.command("start", ctx => ctx.reply("Welcome to TapAI bot!"));
+
+// All handlers here...
+
+
+bot.start(); // ← Ka saka nan kawai
+
+// Render Keep Alive Server
 const express = require("express");
 const app = express();
 
