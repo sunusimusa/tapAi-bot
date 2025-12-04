9be67163d0c6bf010fdad58ba9c6d55ce322bfd8
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-04 20:28:17 +0100

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Keep Alive server for Render
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("TapAI Bot is Running");
+});
+
 app.listen(3000, () => {
-  console.log("Bot server running on port 3000");
+    console.log("Web server running on port 3000");
 });
