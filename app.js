fb18366a52d00876606816fe1b04eba6ca0ec8e4
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-05 07:26:39 +0100

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ===============================
 
 const { Bot } = require("grammy");
-const = require("express");
+const express = require("express");
 
 // Load BOT TOKEN
 const bot = new Bot(process.env.BOT_TOKEN);
